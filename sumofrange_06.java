5df5adb8496846ec400fe80ad0a4095d94eb3340
Ayush vaidya <thevaidya45@gmail.com>
2026-06-20 22:50:28 +0530

    Sum of Range

---
+++ sumofrange_06.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class sumofrange_06 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n=sc.nextInt();
+        System.out.println("Enter the starting range");
+        int start=sc.nextInt();
+        System.out.println( "Enter the end range");;
+        int end=sc.nextInt();
+        int sum=0;
+        int arr[]=new int[n];
+        System.out.println("ENter array elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        if(start>=0 && end<=n){
+            for(int i=start;i<=end;i++){
+                sum+=arr[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}

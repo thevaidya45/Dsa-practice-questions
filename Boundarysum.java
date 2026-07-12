0d0e4d8c1909b1d86ea6e16e828c420d2f95a2e4
Ayush vaidya <thevaidya45@gmail.com>
2026-07-13 01:10:17 +0530

    Boundary sum

---
+++ Boundarysum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class   {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int sum=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i==0 || i==r-1 || j==0 || j==c-1){
+                    sum+=arr[i][j];
+                }
+            }
+        }
+        System.out.println("Sum =" +sum);
+    }
+}

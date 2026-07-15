8e6da2b8d8bf23384c78f2c708ae2ff42dcd39d6
Ayush vaidya <thevaidya45@gmail.com>
2026-07-15 22:50:21 +0530

    random

---
+++ FirstNonRepeating.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class FirstNonRepeating {
+    public static void main(String[] args) {
+        String str = "swiss";
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println("First non-repeating character: " + entry.getKey());
+                return;
+            }
+        }
+
+        System.out.println("No non-repeating character found.");
+    }
+}

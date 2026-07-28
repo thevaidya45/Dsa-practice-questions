11fbedf67a0ff8c76820178895a53999fa3e4ac6
Ayush vaidya <thevaidya45@gmail.com>
2026-07-28 21:14:18 +0530

    Simple stack push

---
+++ stack1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class stack1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Stack<Integer> s=new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s);
+    }
+}

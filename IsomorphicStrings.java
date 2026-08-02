d22a6717cc8df1671d988177cab4a2debaa817b1
Ayush vaidya <thevaidya45@gmail.com>
2026-08-02 22:28:37 +0530

    random

---
+++ IsomorphicStrings.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class IsomorphicStrings {
+
+    public static boolean isIsomorphic(String s, String t) {
+
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if (mapST.containsKey(ch1)) {
+                if (mapST.get(ch1) != ch2) {
+                    return false;
+                }
+            } else {
+                mapST.put(ch1, ch2);
+            }
+
+            if (mapTS.containsKey(ch2)) {
+                if (mapTS.get(ch2) != ch1) {
+                    return false;
+                }
+            } else {
+                mapTS.put(ch2, ch1);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String s = sc.next();
+
+        System.out.print("Enter second string: ");
+        String t = sc.next();
+
+        System.out.println(isIsomorphic(s, t));
+
+        sc.close();
+    }
+}

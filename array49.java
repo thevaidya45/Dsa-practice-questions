ff3b247e1c26168fb6abcf5813c46cadb0146773
Ayush vaidya <thevaidya45@gmail.com>
2026-08-12 21:52:52 +0530

    Matrix spriral traversal

---
+++ array49.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class array49 {
+
+    public static void spiral(int[][] matrix) {
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int j = left; j <= right; j++) {
+                System.out.print(matrix[top][j] + " ");
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    System.out.print(matrix[bottom][j] + " ");
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        spiral(matrix);
+
+        sc.close();
+    }
+}
